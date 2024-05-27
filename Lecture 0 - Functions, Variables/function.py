b240b6f5c0a08b4123b802eae5a66e1d813f4db8
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-05-27 23:05:19 +0530

    python files updated

---
+++ Lecture 0 - Functions, Variables/function.py
@@ -0,0 +1,20 @@
+# function in python
+def name_setter():
+    first_name = input("Enter your first name: ")
+    middle_name = input("Enter your middle name: ")
+    last_name = input("Enter your last name: ")
+    print(first_name, middle_name, sep=" ", end=" ")  # sep and end are Named parameters
+    print(last_name)
+
+
+def form():
+    name_setter()
+    department_setter()
+
+
+def department_setter():
+    dep = input("Select your department: ")
+    print(dep)
+
+
+form()
