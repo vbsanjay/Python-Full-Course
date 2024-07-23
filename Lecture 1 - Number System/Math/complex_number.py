70645b6d115d4949490f1be5140419d0d51c73ba
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-23 12:18:24 +0530

    math on complex numbers have been performed

---
+++ Lecture 1 - Number System/Math/complex_number.py
@@ -0,0 +1,17 @@
+# Define two complex numbers
+complex_num1 = 2 + 3j
+complex_num2 = 1 - 4j
+
+# Perform operations
+addition = complex_num1 + complex_num2
+subtraction = complex_num1 - complex_num2
+multiplication = complex_num1 * complex_num2
+division = complex_num1 / complex_num2
+
+# Print results
+print("Complex number 1:", complex_num1)
+print("Complex number 2:", complex_num2)
+print("\nAddition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
