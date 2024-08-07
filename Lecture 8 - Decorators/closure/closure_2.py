73e412d415339f898b1162c29cbd35d9c2b472d6
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-07 14:48:06 +0530

    closure + prerequisite of decorators

--- Lecture 8 - Decorators/closure/closure_2.py
+++ Lecture 8 - Decorators/closure/closure_2.py
@@ -50,3 +50,49 @@ avg = average()
 avg(1)
 res = avg(19)
 print("using closure(optimized):",res)
+
+# My observation is, when a class need to perform only one function, closure can be considered
+
+# Write a closure to calculate the number of times a function is called
+
+c = dict()
+def adder(a: int, b: int) -> int:
+    return a + b
+
+def mult(a: int, b: int) -> int:
+    return a * b
+
+def div(a: int, b: int) -> int:
+    return a // b
+
+def counter(func, counters):
+    count = 0
+    def inner(*args, **kwargs):
+        nonlocal count
+        count += 1
+        counters[func.__name__] = count
+        return func(*args, **kwargs)
+
+    return inner
+
+
+counter_add = counter(adder,c)
+print(counter_add(1,2))
+print(counter_add(4,2))
+print(counter_add(7,6))
+counter_mult = counter(mult,c)
+print(counter_mult(2,5))
+print(counter_mult(10,2))
+
+print(c)
+
+print(div(10,2)) # 5
+print(c) # {'adder': 3, 'mult': 2}
+# now you can have the name of variable as same a function and observe how it works
+
+div = counter(div,c)
+
+# now we 'div' is used but with the extra functionality provided by the closure 
+
+print(div(10,2)) # 5
+print(c) # {'adder': 3, 'mult': 2, 'div': 1}
